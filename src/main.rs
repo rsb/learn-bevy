2e43530d2102c684b9d78ccc2802c85c27cb1602
Robert Scott-Buccleuch <60840+rsb@users.noreply.github.com>
2023-07-16 11:16:28 -0700

    ch2 - First Game Setup - finished

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 use bevy::prelude::*;
 
 fn main() {
-  App::new().add_plugins(DefaultPlugins).run();
+  App::new()
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, setup)
+    .run();
+}
+
+#[derive(Component)]
+pub struct Player {}
+
+pub fn setup(mut cmds: Commands, asset_server: Res<AssetServer>) {
+  cmds.spawn(Camera2dBundle::default());
+
+  cmds.spawn((
+    SpriteBundle {
+      texture: asset_server.load("sprites/ball_blue_large.png"),
+      ..default()
+    },
+    Player {},
+  ));
 }
